3ab65b6044eeda445e05146740fd88051632577a
Ramiro Gordon <ramirogordon2@gmail.com>
2023-09-04 23:54:31 -0300

    calculateCeoefficients

--- src/comms-interpreter/comms-interpreter.service.ts
+++ src/comms-interpreter/comms-interpreter.service.ts
@@ -114,24 +114,6 @@ export class CommsInterpreterService {
     return decodedMessage.join(' ');
   }
 
-  private calculateCeoefficients(
-    satellite_A: number[],
-    satellite_B: number[],
-  ): { A: number; B: number } {
-    const A =
-      (satellite_A[1] - satellite_B[1]) / (satellite_A[0] - satellite_B[0]);
-    const B =
-      (-Math.pow(satellite_A[0], 2) -
-        Math.pow(satellite_A[1], 2) +
-        Math.pow(satellite_B[0], 2) +
-        Math.pow(satellite_B[1], 2) +
-        Math.pow(satellite_A[2], 2) -
-        Math.pow(satellite_B[2], 2)) /
-      (2 * satellite_A[0] - 2 * satellite_B[0]);
-
-    return { A, B };
-  }
-
   private getLocation(distances: number[]): number[] | number[][] | null {
     const distancesLength = distances.filter(
       (value) => typeof value === 'number',
@@ -263,5 +245,23 @@ export class CommsInterpreterService {
         [...satoLocation, satoDistance],
       ];
     }
+  }
+
+  private calculateCeoefficients(
+    satellite_A: number[],
+    satellite_B: number[],
+  ): { A: number; B: number } {
+    const A =
+      (satellite_A[1] - satellite_B[1]) / (satellite_A[0] - satellite_B[0]);
+    const B =
+      (-Math.pow(satellite_A[0], 2) -
+        Math.pow(satellite_A[1], 2) +
+        Math.pow(satellite_B[0], 2) +
+        Math.pow(satellite_B[1], 2) +
+        Math.pow(satellite_A[2], 2) -
+        Math.pow(satellite_B[2], 2)) /
+      (2 * satellite_A[0] - 2 * satellite_B[0]);
+
+    return { A, B };
   }
 }
