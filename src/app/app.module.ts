b19d6f2ff14648d2b40ed633e1f3beb17a5e5bb8
Ramiro Gordon <ramirogordon2@gmail.com>
2023-09-07 14:22:48 -0300

    update main.ts with absolute imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from '../middleware/LoggerMiddleware';
-import { CommsInterpreterModule } from '../comms-interpreter/comms-interpreter.module';
+import { CommsInterpreterModule } from '@comms-interpreter/comms-interpreter.module';
 
 @Module({
   imports: [CommsInterpreterModule],
